0141a73a2ef336d30a33a3fce730f22fd40384b5
Lekksii <alexkudryawtsev@gmail.com>
2022-03-05 15:13:24 +0200

    Added animation list from JSON, reading data from profile file

--- actor.py
+++ actor.py
@@ -1,37 +1,29 @@
 from ursina import *
 from direct.actor.Actor import Actor
 from panda3d.core import SamplerState
+import my_json
 
 class PitoActor(Entity):
-    def __init__(self,model,texture,gasmask=False,weapon="w_ak",idle_anim="idle",weapon_hold="r_hand", **kwargs):
+    def __init__(self,profile_id, **kwargs):
+        #model,texture,gasmask=False,weapon="w_ak",idle_anim="idle",weapon_hold="r_hand"
         super().__init__()
-        self.animations = {
-            "idle": "assets/models/anim/pito_npc-idle",
-            "idle_arm": "assets/models/anim/pito_npc-idle_arm",
-            "idle_cross": "assets/models/anim/pito_npc-idle_cross",
-            "idle_weapon": "assets/models/anim/pito_npc-idle_weapon",
-            "idle_sit": "assets/models/anim/pito_npc-idle_sit",
-            "idle_sit_ground": "assets/models/anim/pito_npc-idle_sit_ground"
-        }
+        self.keys = {}
+        self.profile = my_json.read("assets/creatures/characters")[profile_id]
+        self.anims = my_json.read("assets/creatures/animations")
+
+        self.npc_model = self.profile["model"]
+        self.npc_texture = "assets/textures/"+self.profile["texture"]+".png"
+        self.gasmask = self.profile["gasmask"]
+        self.npc_weapon = self.profile["weapon"]
+        self.weapon_hold = self.profile["weapon_hold"]
+        self.idle_anim = self.profile["idle_animation"]
+
         self.actor = Actor(
-            {"body": "assets/models/"+model,
+            {"body": "assets/models/"+self.npc_model,
              "backpack": "assets/models/anim/npc_backpack"}, {
-                "body": {
-                    "idle": "assets/models/anim/pito_npc-idle",
-                    "idle_arm": "assets/models/anim/pito_npc-idle_arm",
-                    "idle_cross": "assets/models/anim/pito_npc-idle_cross",
-                    "idle_weapon": "assets/models/anim/pito_npc-idle_weapon",
-                    "idle_sit": "assets/models/anim/pito_npc-idle_sit",
-                    "idle_sit_ground": "assets/models/anim/pito_npc-idle_sit_ground"
-                },
-                "backpack": {
-                    "idle": "assets/models/anim/pito_npc-idle",
-                    "idle_arm": "assets/models/anim/pito_npc-idle_arm",
-                    "idle_cross": "assets/models/anim/pito_npc-idle_cross",
-                    "idle_weapon": "assets/models/anim/pito_npc-idle_weapon",
-                    "idle_sit": "assets/models/anim/pito_npc-idle_sit",
-                    "idle_sit_ground": "assets/models/anim/pito_npc-idle_sit_ground"
-                }})
+                "body": self.anims,
+                "backpack": self.anims
+            })
         self.texture_bp = application.base.loader.loadTexture("assets/textures/texobj2.png")
 
         self.r_hand = self.actor.exposeJoint(None,"body","r_arm_1")
@@ -40,12 +32,12 @@ class PitoActor(Entity):
         self.head = self.actor.exposeJoint(None, "body", "head")
 
         # r_hand, l_hand, #backpack
-        self.weapon_place = weapon_hold
-        self.antigas = gasmask
-        self.antigas_type = "antigas" # standard, elite
+        self.weapon_place = self.weapon_hold
+        self.antigas = self.gasmask
+        self.antigas_type = self.profile["gasmask_type"] # antigas, elite
 
         self.weapon_scale = 2.5
-        self.weapon_model = "assets/models/w_ak"
+        self.weapon_model = "assets/models/"+self.npc_weapon
         self.weapon_tex = "assets/textures/texobj2.png"
 
         self.weapon = Entity(position=0, rotation=0, scale=self.weapon_scale,
@@ -72,15 +64,15 @@ class PitoActor(Entity):
             self.antigas_model.texture = texture
 
         if self.weapon_place == "backpack":
-            changeWeapon("assets/models/" + weapon, self.weapon_tex)
+            changeWeapon("assets/models/" + self.npc_weapon, self.weapon_tex)
             stickToBone(self.backpack,(0.4,-0.5, 0.5),(0, 0, 15))
 
         if self.weapon_place == "l_hand":
-            changeWeapon("assets/models/" + weapon, self.weapon_tex)
+            changeWeapon("assets/models/" + self.npc_weapon, self.weapon_tex)
             stickToBone(self.l_hand,(-0.05, 0.15, 0.9),(-15, -5, 15))
 
         if self.weapon_place == "r_hand":
-            changeWeapon("assets/models/"+weapon,self.weapon_tex)
+            changeWeapon("assets/models/"+self.npc_weapon,self.weapon_tex)
             stickToBone(self.r_hand,(-0.05, 0.15, 0.9),(-15, -5, 15))
 
         if self.antigas:
@@ -88,8 +80,9 @@ class PitoActor(Entity):
             setAntigas(self.head,(0, 0.3, 0),(-90, 180, 0))
 
         self.actor.reparentTo(self)
-        self.texture_npc = application.base.loader.loadTexture(texture)
+        self.texture_npc = application.base.loader.loadTexture(self.npc_texture)
 
+        self.collider = BoxCollider(self,center=Vec3(0,1.2,0),size=Vec3(0.3,1.2,0.3))
 
         self.new_tex = self.actor.find("__Actor_body").findTextureStage('pstalker.png')
         self.new_bp = self.actor.find("__Actor_backpack").findTextureStage('texobj2.png')
@@ -100,7 +93,7 @@ class PitoActor(Entity):
         self.actor.find("__Actor_body").setTexture(self.new_tex, self.texture_npc, 1)
         self.actor.find("__Actor_backpack").setTexture(self.new_bp, self.texture_bp, 1)
 
-        self.actor.loop(idle_anim)
+        self.actor.loop(self.idle_anim)
 
         for key, value in kwargs.items ():
             setattr (self, key, value)
