975e4bfc45a103cfda871a93559a33c422b59e0e
Lekksii <alexkudryawtsev@gmail.com>
2022-05-01 14:30:15 +0300

    Deleted PDA button on/off function in pause menu

--- story.py
+++ story.py
@@ -1,14 +1,15 @@
 import game
 
+def add_pda_marker(id):
+    game.get_player().pause_menu.pda_window.add_marker(id)
+
 def forest_road_quest():
     if game.get_player().quests.get_quest("quest_rusty_0"):
         game.get_player().quests.get_quest("quest_rusty_0").complete()
-        game.get_player().pause_menu.pda_window.add_marker("village_marker")
-        game.enable_pda_in_pause(True)
+        add_pda_marker("village_marker")
 
 def first_battle():
     game.get_player().quests.add_quest("quest_find_camp")
-    game.enable_pda_in_pause(True)
 
 def first_in_village():
     if game.get_player().quests.has_quest("quest_find_camp"):
